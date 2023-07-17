2e1f480d5aa3bbbea1eb2fe151e56d90e428aa91
azsx92 <azsx92@gmail.com>
2023-07-17 15:00:59 +0900

    p258 4.2chap

---
+++ src/main/java/org/zerock/springex/mapper/TimeMapper.java
@@ -0,0 +1,11 @@
+package org.zerock.springex.mapper;
+
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+public interface TimeMapper {
+
+    @Select("select now()")
+    String getTime();
+
+}
